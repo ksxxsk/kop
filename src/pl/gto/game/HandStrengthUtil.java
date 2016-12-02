9baf4350474205468a5b06e8f9acd333205a0fda
ksxxsk <ksh4k@go2.pl>
2016-12-02 13:11:59 +0100

    handVsRange: 1. now takes range as Set to avoid duplicates 2. return equity instead of win%

--- src/pl/gto/game/HandStrengthUtil.java
+++ src/pl/gto/game/HandStrengthUtil.java
@@ -5,6 +5,7 @@ import pl.gto.card.Card;
 import pl.gto.hand.Hand;
 import pl.gto.hand.HandComparator;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
@@ -17,17 +18,16 @@ public class HandStrengthUtil {
 
     private final static HandComparator handComparator = new HandComparator();
 
-    public static double handVsRange(Hand hand, Collection<Hand> range, Collection<Card> dealtCards) {
+    public static double handVsRange(Hand hand, Set<Hand> range, Collection<Card> dealtCards) {
 
-        Set<Hand> filteredRange = range.stream().filter(h -> !h.containsAnyOf(dealtCards)).collect(Collectors.toSet());
-        int win = filteredRange.stream().mapToInt( h -> handComparator.compare(hand, h) > 0 ? 1 : 0).sum();
+        int[] comparision = range.stream().filter(h -> !h.containsAnyOf(dealtCards)).mapToInt( h -> handComparator.compare(hand, h)).toArray();
+        long win = Arrays.stream(comparision).filter( i -> i > 0).count();
+        long draw = (int) Arrays.stream(comparision).filter(i -> i == 0).count();
 
-        System.out.println(win);
-        System.out.println(filteredRange.size());
-        return (double) win / filteredRange.size();
+        return ( 0.5 * draw + win ) / comparision.length;
     }
 
-    public static double rangeVsRange(Collection<Hand> heroRange, Collection<Hand> villainRange) {
+    public static double rangeVsRange(Collection<Hand> heroRange, Set<Hand> villainRange) {
         double sum = 0;
         for (Hand hand : heroRange) {
             sum += handVsRange(hand, villainRange, Collections.emptySet());
